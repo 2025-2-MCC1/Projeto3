2821c9a5181a63aa1e4da449b9b6c7ed6180ba80
Guilherme Augusto <guiaugusth060@gmail.com>
2025-11-03 10:42:03 -0300

    Correcao do respown

--- Projeto_Oficial/Memoria_Pirata/Assets/Scripts/CardSpawner.cs
+++ Projeto_Oficial/Memoria_Pirata/Assets/Scripts/CardSpawner.cs
@@ -17,83 +17,119 @@ public class CardRandomizer : MonoBehaviour
     private Transform[] slots;
     int rnd;
     int cont;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+    int f = 0;
+    int g = 0;
+    int h = 0;
+    int j = 0;
 
     void Start()
     {
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
-        int e = 0;
-        int f = 0;
-        int g = 0;
-        int h = 0;
-        int j = 0;
+        
         Debug.Log("spawCarta");
        // StartCoroutine(SetupCards());
         for (int i = 0; i < slots.Length; i++)
         {
             
 
-            rnd = Random.Range(0, cards.Length - 1);
-            Debug.Log("rnd: " +  rnd);  
-            switch (rnd)
-            {
-                
-                case 0:
-                    a++;
-                    if (a >2) { Debug.Log("carta 0 ja foi"); return; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 1:
-                    b++;
-                    if (b > 2) { Debug.Log("carta 1 ja foi"); return; ; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 2:
-                    c++;
-                    if (c > 2) { Debug.Log("carta 0 ja foi"); return; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 3:
-                    d++;
-                    if (d > 2) { Debug.Log("carta 1 ja foi"); return; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 4:
-                    e++;
-                    if (e > 2) { Debug.Log("carta 0 ja foi"); return; ; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 5:
-                    f++;
-                    if (f > 2) { Debug.Log("carta 1 ja foi"); return; ; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 6:
-                    g++;
-                    if (g > 2) { Debug.Log("carta 0 ja foi"); return; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 7:
-                    h++;
-                    if (h > 2) { Debug.Log("carta 1 ja foi"); return; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                case 8:
-                    j++;
-                    if (j > 2) { Debug.Log("carta 1 ja foi"); return; }
-                    Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
-                    break;
-                    default:
-                        Debug.LogError("Não criei cartas");
-                        break;
-                        
-
-
-            }           
+           GerarCartas(i);
+           
             
         }
+    }
+
+    void GerarCartas(int i)
+    {
+        bool cartaGerada = false;
+
+        while (!cartaGerada)
+        {
+            rnd = Random.Range(0, cards.Length); // note que aqui vai até cards.Length, não -1
+
+            switch (rnd)
+            {
+                case 0:
+                    if (a < 2)
+                    {
+                        a++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 1:
+                    if (b < 2)
+                    {
+                        b++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 2:
+                    if (c < 2)
+                    {
+                        c++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 3:
+                    if (d < 2)
+                    {
+                        d++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 4:
+                    if (e < 2)
+                    {
+                        e++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 5:
+                    if (f < 2)
+                    {
+                        f++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 6:
+                    if (g < 2)
+                    {
+                        g++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 7:
+                    if (h < 2)
+                    {
+                        h++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+                case 8:
+                    if (j < 2)
+                    {
+                        j++;
+                        Instantiate(cards[rnd], slots[i].transform.position, Quaternion.identity);
+                        cartaGerada = true;
+                    }
+                    break;
+            }
+        
+
+
+
+}
     }
 
     IEnumerator SetupCards()
